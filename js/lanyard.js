27994535b17556543d3aa90f7875d3e4b62345db
ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2023-11-21 13:02:27 -0800

    Update lanyard.js

--- js/lanyard.js
+++ js/lanyard.js
@@ -1,3 +1,11 @@
+let DLog = false;
+console.log('Dexrn: I put logging in here but you\'ll have to set \"DLog\" to true.')
+function DexrnsFunnyLogger(message) {
+	if (DLog) {
+		console.log('in lanyard.js: ' + message);
+	}
+}
+
 const API_URL = 'https://api.lanyard.rest/v1';
 const USERID = '485504221781950465';
 const pfp = document.getElementById('pfp');
@@ -21,6 +29,8 @@ const elapsed = document.getElementById('activity-elapsed');
 var disc_status;
 var disc_platform;
 var disc_isOffline;
+DexrnsFunnyLogger('Dexrn: I hate this so I am adding debug logging here...');
+DexrnsFunnyLogger('variables set');
 async function fetchResponse(userId) {
     try {
         const res = await fetch(`${API_URL}/users/${userId}`);
@@ -39,6 +49,7 @@ async function setAvatar() {
     } = await fetchResponse(USERID);
     const fullUrl = `https://cdn.discordapp.com/avatars/${USERID}/${avatar}.webp?size=512`;
     pfp.src = fullUrl;
+    DexrnsFunnyLogger('setAvatar');
     // pfp2.src = fullUrl;
 }
 async function setAvatarFrame() {
@@ -54,6 +65,7 @@ async function setAvatarFrame() {
     // Dexrn: Jank incoming!
     switch (discord_status) {
     case 'online':
+	 DexrnsFunnyLogger('Online');
         statusDot.style.background =
             '#3ba45d';
         status2.innerHTML = 'Online';
@@ -63,6 +75,7 @@ async function setAvatarFrame() {
         status3.style.cssText = 'color: #3ba45d; opacity: 1;';
         break;
     case 'dnd':
+	 DexrnsFunnyLogger('DND (Do not disturb)');
         statusDot.style.background =
             '#ed4245';
 	 pfp.style.border = '2px solid #ed4245';
@@ -72,6 +85,7 @@ async function setAvatarFrame() {
         status3.style.cssText = 'color: #ed4245; opacity: 1;';
         break;
     case 'idle':
+	 DexrnsFunnyLogger('Idle');
         statusDot.style.background =
             '#faa81a';
         status2.innerHTML = 'Idle';
@@ -81,6 +95,7 @@ async function setAvatarFrame() {
         status3.style.cssText = 'color: #faa81a; opacity: 1;';
         break;
     case 'offline':
+	 DexrnsFunnyLogger('Offline');
         statusDot.style.background =
             '#747e8c';
         statusDot.title = 'Offline';
@@ -90,20 +105,33 @@ async function setAvatarFrame() {
         status2.style.cssText = 'color: unset; opacity: 0.5;';
         disc_isOffline = true;
         break;
+    default:
+        DexrnsFunnyLogger('Unknown (default)');
+	 statusDot.style.background =
+            '#747e8c';
+        statusDot.title = 'Unknown';
+        status2.innerHTML = "Unknown";
+	 pfp.style.border = '2px solid #747e8c';
+        pfp.style.boxShadow = '0 0 20px #747e8c';
+        status2.style.cssText = 'color: unset; opacity: 0.5;';
+        disc_isOffline = true;
     }
 
 const platformarray = [];
 
 // Dexrn: I should make it show pictures instead.
 if (active_on_discord_desktop == true) {
-    platformarray.push(" Desktop");
+	DexrnsFunnyLogger('Desktop');
+       platformarray.push(" Desktop");
 }
 
 if (active_on_discord_mobile == true) {
+    DexrnsFunnyLogger('Mobile');
     platformarray.push(" Mobile");
 }
 
 if (active_on_discord_web == true) {
+    DexrnsFunnyLogger('Web');
     platformarray.push(" Web");
 }
 
@@ -132,6 +160,7 @@ async function setStatus() {
     }
 
     if (activities) {
+	 DexrnsFunnyLogger('activities is true');
         const activityOfType4 = activities.find(m => m.type == 4);
         if (activityOfType4) {
             const { state } = activityOfType4;
@@ -151,10 +180,12 @@ async function setActivityBigImage() {
     } = await fetchResponse(USERID);
     const mostRecent = activities.filter(m => m.type !== 4).shift();
     if (!mostRecent?.assets?.large_image) {
+	 DexrnsFunnyLogger('No large_image');
         bigImage.style.display = 'none';
         return;
     }
     if (mostRecent.assets.large_image.includes("spotify")) {
+	 DexrnsFunnyLogger('spotify');
         bigImage.style.display = 'block';
         bigImage.src = spotify.album_art_url;
 		bigImage.title =  spotify.album;
@@ -163,6 +194,7 @@ async function setActivityBigImage() {
     }
     const imageLink = mostRecent.assets.large_image.includes("external") ? `https://media.discordapp.net/external/${mostRecent.assets.large_image.split("mp:external/")[1]}` :  `https://cdn.discordapp.com/app-assets/${mostRecent.application_id}/${mostRecent.assets.large_image}.png?size=256`;
     bigImage.style.display = 'block';
+    DexrnsFunnyLogger('large_image set');
     bigImage.src = imageLink;
 	bigImage.title = mostRecent.assets.large_text;
 }
@@ -177,20 +209,20 @@ async function setActivitySmallImage() {
         smallImage.style.display = 'none';
         return;
     }
+	DexrnsFunnyLogger('small_image true');
     const imageLink = mostRecent.assets.small_image.includes("external") ? `https://media.discordapp.net/external/${mostRecent.assets.small_image.split("mp:external/")[1]}` : `https://cdn.discordapp.com/app-assets/${mostRecent.application_id}/${mostRecent.assets.small_image}.png?size=256`;
-        if (!mostRecent.assets.large_image && mostRecent.assets.small_image) {
+        if (!mostRecent.assets.large_image) {
         smallImageAlt.style.display = 'block';
         smallImageAlt.src = imageLink;
         smallImageAlt.title = mostRecent.assets.small_text;
         smallImage.style.display = 'none';
-	 console.log(mostRecent.assets.large_image)
-	console.log('show small image as large.')
+	DexrnsFunnyLogger('show small image as large.');
 	} else {
 	smallImageAlt.style.display = 'none';
 	smallImage.style.display = 'block';
        smallImage.src = imageLink;
 	smallImage.title = mostRecent.assets.small_text;
-	console.log('show small image on top of large image.')
+	DexrnsFunnyLogger('show small image on top of large image.');
 }}
 async function setActivityName() {
     const {
@@ -200,9 +232,11 @@ async function setActivityName() {
     } = await fetchResponse(USERID);
     const mostRecent = activities.filter(m => m.type !== 4).shift();
     if (!mostRecent?.name) {
+	 DexrnsFunnyLogger('No activity name');
         name.innerHTML = 'No activity';
         return;
     }
+    DexrnsFunnyLogger('activity name set');
     name.style.display = 'block';
     name.innerHTML = mostRecent.name;
 }
