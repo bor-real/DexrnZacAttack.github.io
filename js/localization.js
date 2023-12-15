1e50e2a277cf6d054ae88326114b4be444b9c824
ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2023-12-15 00:53:04 -0800

    Moved the localization script into it's own file and made other things use the localization script

---
+++ js/localization.js
@@ -0,0 +1,80 @@
+function checkLang() {
+    const lang = getLanguageFromCookie();
+    console.log(lang);
+    let langFilePath;
+
+    switch (lang) {
+    case 'zh-CN':
+        langFilePath = "/assets/lang/zh-CN.json";
+        break;
+    case 'en-US':
+        langFilePath = "/assets/lang/en-US.json";
+        break;
+    default:
+        langFilePath = "/assets/lang/en-US.json";
+        break;
+    }
+    setLang(langFilePath);
+}
+
+checkLang();
+
+
+function getLanguageFromCookie() {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        const [name, value] = cookie.trim().split('=');
+        if (name === 'lang') {
+            return value;
+        }
+    }
+    return null; // Cookie not found
+}
+
+
+// Dexrn: Localization! (Kinda janky.)
+// Dexrn: TODO: Make this it's own JS file.
+function setLang(langFilePath) {
+    fetch(langFilePath)
+        .then(response => response.json())
+        .then(data => {
+            checkIfExists('loadingtext', data.LoadingText);
+            checkIfExists('status2', data.Status2);
+            checkIfExists('activity-name', data.ActivityName);
+            checkIfExists('activity-state', data.ActivityState);
+            checkIfExists('activity-detail', data.ActivityDetail);
+            checkIfExists('activity-path', data.ActivityPath);
+            checkIfExists('home-path', data.HomePath);
+            checkIfExists('about-path', data.AboutPath);
+            checkIfExists('stuff-path', data.LinksPath);
+            checkIfExists('mainbtn-1', data.MainPageButton1);
+            checkIfExists('mainbtn-2', data.MainPageButton2);
+            checkIfExists('abm-1', data.AboutMeTxt1);
+            checkIfExists('abm-2', data.AboutMeTxt2);
+            checkIfExists('savebtn', data.SettingsSaveButton);
+            checkIfExists('settings-path', data.SettingsPath);
+            checkIfExists('backbtn', data.BackButton);
+            checkIfExists('languagetxt', data.LanguageText);
+            checkIfExists('settingsbtn', data.SettingsButton);
+            checkIfExists('ftlLanguageTxt', data.FirstTimeLoadLanguageText);
+            checkIfExists('ftlcbtn-1', data.FirstTimeLoadButton1);
+            checkIfExists('backbtn', data.BackButton);
+            checkIfExists('backbtn2', data.BackButtonAlt);
+            checkIfExists('qmghp-path', data.QMGHeaderParserPath);
+            checkIfExists('fileselectbtn', data.FileSelectButton);
+            checkIfExists('output', data.QMGHPOutputText);
+            checkIfExists('qmgr-path', data.QMGResearchPath);
+            checkIfExists('404msg', data.NotFoundErrorText)
+            checkIfExists('homebtn', data.HomeButton);
+            checkIfExists('403msg', data.ForbiddenErrorText)
+        })
+        .catch(error => console.error('Error whilst loading lang file:', error));
+}
+
+// Dexrn: This makes sure that the element exists before setting it... otherwise it will throw an error.
+function checkIfExists(elementId, value) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.textContent = value;
+    }
+}
