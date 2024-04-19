fa019f8ce792c2cf770a8c5c321bebae682519a5
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-04-19 01:26:05 -0700

    Multi-Page App!
    Figured out the CSS resolutions too.
    
    https://vitejs.dev/guide/build#multi-page-app

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./",
   build: {
-    target: "esnext"
+    target: "esnext",
+    rollupOptions: {
+      input: [
+        "index.html",
+        "QMG/index.html",
+        "QMGHeaderParser/index.html",
+        "SGExtractor/index.html",
+        "403/index.html",
+        "404/index.html",
+      ]
+    }
   },
   server: {
     port: 5500,
